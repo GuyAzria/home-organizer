d0346f90300f0e9d893c025a6a6ad153d2b64c82
GuyAzria <119097035+GuyAzria@users.noreply.github.com>
2026-01-14 06:53:52 +0200

    Update organizer-icon.js

--- custom_components/home_organizer/frontend/organizer-icon.js
+++ custom_components/home_organizer/frontend/organizer-icon.js
@@ -1,4 +1,4 @@
-// ATTACH TO GLOBAL WINDOW OBJECT
+// NO 'export' allowed here! We use window.OrganizerAssets
 window.OrganizerAssets = {
   ICONS: {
     arrow_up: '<svg viewBox="0 0 24 24"><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>',
